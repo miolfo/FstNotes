24daac1619983097f1ca6de6a9029cc1ad9e096c
Mikko Forsman <mikko.o.forsman@gmail.com>
2016-10-25 18:53:17 +0300

    Progress on widget

--- app/src/main/java/com/example/forge/fstnotes/FstNoteWidgetProvider.java
+++ app/src/main/java/com/example/forge/fstnotes/FstNoteWidgetProvider.java
@@ -19,6 +19,19 @@ import java.util.ArrayList;
 
 public class FstNoteWidgetProvider extends AppWidgetProvider {
 
+    public static final String ADD_NOTE_ACTION = "com.example.forge.fstnotes.ADD_NOTE_ACTION";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction().equals(ADD_NOTE_ACTION)){
+            Intent startedIntent = new Intent(context, MainActivity.class);
+            context.startActivity(startedIntent);
+        }
+
+        super.onReceive(context, intent);
+
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // update each of the app widgets with the remote adapter
@@ -29,7 +42,6 @@ public class FstNoteWidgetProvider extends AppWidgetProvider {
             Intent intent = new Intent(context, FstNoteWidgetService.class);
             // Add the app widget ID to the intent extras.
             intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
             // Instantiate the RemoteViews object for the app widget layout.
             RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.fstnote_widget);
             // Set up the RemoteViews object to use a RemoteViews adapter.
@@ -37,8 +49,28 @@ public class FstNoteWidgetProvider extends AppWidgetProvider {
             // to a RemoteViewsService  through the specified intent.
             // This is how you populate the data.
             rv.setRemoteAdapter(appWidgetIds[i], R.id.notes_widget_list, intent);
+
+            //Intent addNewIntent = new Intent(context, FstNoteWidgetProvider.class);
+            //addNewIntent.setAction(FstNoteWidgetProvider.ADD_NOTE_ACTION);
+            //addNewIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            //PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, addNewIntent,
+            //        PendingIntent.FLAG_UPDATE_CURRENT);
+            //rv.setPendingIntentTemplate(R.id.add_new_note_widget, toastPendingIntent);
+
+            Intent intent1 = new Intent(ADD_NOTE_ACTION);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent1, PendingIntent.FLAG_UPDATE_CURRENT);
+            rv.setOnClickPendingIntent(R.id.add_new_note_widget, pendingIntent );
+
+            //RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.fstnote_widget);
+            //remoteViews.setOnClickPendingIntent(R.id.add_new_note_widget, getPendingSelfIntent(context, ADD_NOTE_ACTION));
+
             appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
         }
+
+
         super.onUpdate(context, appWidgetManager, appWidgetIds);        Log.i("WidgetProvider", "Widget on update! " + appWidgetIds[0]);
     }
 }
